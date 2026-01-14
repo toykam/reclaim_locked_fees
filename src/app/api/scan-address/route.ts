e2404e9cc14ab153fab13b966477643aa5243265
toykam <kabirtoyyib19@gmail.com>
2026-01-14 15:51:43 +0100

    moved somethigns to the backend

--- src/app/api/scan-address/route.ts
+++ src/app/api/scan-address/route.ts
@@ -2,25 +2,25 @@ import { NextRequest, NextResponse } from "next/server";
 import { Connection, PublicKey } from "@solana/web3.js";
 import { detectReclaimableAccounts } from "@/lib/detection-logic";
 
-const connection = new Connection(process.env.SOLANA_RPC_URL!, { commitment: "confirmed" });
 
 export async function POST(req: NextRequest) {
-  try {
-    const body = await req.json();
-    const { pubkey } = body;
+    try {
+        const connection = new Connection(process.env.SOLANA_RPC_URL!, { commitment: "confirmed" });
+        const body = await req.json();
+        const { pubkey } = body;
 
-    if (!pubkey) return NextResponse.json({ error: "Missing pubkey" }, { status: 400 });
+        if (!pubkey) return NextResponse.json({ error: "Missing pubkey" }, { status: 400 });
 
-    const owner = new PublicKey(pubkey);
+        const owner = new PublicKey(pubkey);
 
-    // Fetch all SPL token accounts for the owner
-    const accounts = await detectReclaimableAccounts(
-        connection,
-        owner
-    )
+        // Fetch all SPL token accounts for the owner
+        const accounts = await detectReclaimableAccounts(
+            connection,
+            owner
+        )
 
-    return NextResponse.json({ accounts: accounts });
-  } catch (err: any) {
-    return NextResponse.json({ error: err.message }, { status: 500 });
-  }
+        return NextResponse.json({ accounts: accounts });
+    } catch (err: any) {
+        return NextResponse.json({ error: err.message }, { status: 500 });
+    }
 }
